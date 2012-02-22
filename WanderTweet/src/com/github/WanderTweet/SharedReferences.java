8c64dccc01e925339fb5eaf0f33979657bb50fd1
Andrew Bennett <carbonnet@gmail.com>
2012-02-22 20:45:22 +0000

    the bare minimum file needed

---
+++ WanderTweet/src/com/github/WanderTweet/SharedReferences.java
@@ -0,0 +1,11 @@
+package com.github.WanderTweet;
+
+import twitter4j.Twitter;
+import android.speech.tts.TextToSpeech;
+
+public abstract class SharedReferences {
+
+	public static TextToSpeech TEXT_TO_SPEECH;
+	public static Twitter TWITTER;
+
+}
